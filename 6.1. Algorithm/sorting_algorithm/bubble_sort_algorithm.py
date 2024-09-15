6d8ffb4e80e9ad57da270baf8bb8cfe6e0feea5e
Angga Dwi Sunarto <kanshinbatosai@gmail.com>
2024-09-15 08:51:10 +0700

    modified:   6.1. Algorithm/sorting_algorithm/bubble_sort_algorithm.py

--- 6.1. Algorithm/sorting_algorithm/bubble_sort_algorithm.py
+++ 6.1. Algorithm/sorting_algorithm/bubble_sort_algorithm.py
@@ -1,9 +1,9 @@
-def bubble_sort(arr):
-    for i in range(len(arr)):
-        for j in range(len(arr) - 1):
-            if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
-    return arr
+def bubble_sort(arr):  # (1)
+    for i in range(len(arr)):  # (2)
+        for j in range(len(arr) - 1):  # (3)
+            if arr[j] > arr[j + 1]:  # (4)
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]  # (5)
+    return arr  # (6)
 
-arr = [64, 34, 25, 12, 22, 11, 90, 1]
-print(bubble_sort(arr))
+arr = [64, 34, 25, 12, 22, 11, 90, 1]  # (7)
+print(bubble_sort(arr))  # (8)
